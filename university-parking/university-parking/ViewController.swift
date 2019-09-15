62cf3ccbb62ff88e09a5d80dca9c27677825d293
Arthur De Araujo <wafflez180@yahoo.com>
2019-09-15 13:38:34 -0400

    Add populated tableView, largeTitle navBar, and UB centered mapView

--- university-parking/university-parking/ViewController.swift
+++ university-parking/university-parking/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let centerCoord = CLLocationCoordinate2D.init(latitude: 43.000877, longitude: -78.787342)
+        mapView.setCamera(MKMapCamera.init(lookingAtCenter: centerCoord, fromEyeCoordinate: centerCoord, eyeAltitude: 1500), animated: false);
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (indexPath.row == 0){
+            let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "1")
+            cell.textLabel?.text = "Hochstetter Lot A"
+            return cell
+        } else if (indexPath.row == 1) {
+            let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "1")
+            cell.textLabel?.text = "Hochstetter Lot B"
+            return cell
+        } else if (indexPath.row == 2) {
+            let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "1")
+            cell.textLabel?.text = "Hochstetter Lot C"
+            return cell
+        } else if (indexPath.row == 3) {
+            let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "1")
+            cell.textLabel?.text = "Cooke Lot A"
+            return cell
+        } else if (indexPath.row == 4) {
+            let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "1")
+            cell.textLabel?.text = "Cooke Lot B"
+            return cell
+        } else {
+            let cell = UITableViewCell.init(style: .subtitle, reuseIdentifier: "1")
+            cell.textLabel?.text = "Greiner Lot A"
+            return cell
+        }
+    }
 
 }
 
