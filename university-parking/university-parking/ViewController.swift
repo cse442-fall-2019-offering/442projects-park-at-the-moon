2ba341293f943ac88b4ddf821760d2c292dbd255
Arthur De Araujo <wafflez180@yahoo.com>
2019-09-15 12:58:26 -0400

    initialize xcode proj

---
+++ university-parking/university-parking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  university-parking
+//
+//  Created by Arthur De Araujo on 9/15/19.
+//  Copyright © 2019 Arthur De Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
