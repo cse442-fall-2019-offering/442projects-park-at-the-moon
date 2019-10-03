25af740cd271b4f3fbdd5883d6195d5448c9e8ce
Sanchit <sanchitcop19@Gmail.com>
2019-10-03 14:04:35 -0400

    Moved requirements one dir up, removed submodule refs and added actual lot data #40

---
+++ patm-server-bk/store.py
@@ -0,0 +1,103 @@
+from json import JSONEncoder
+from collections import UserDict
+
+
+class Store(UserDict):
+
+    def __init__(self):
+        #TODO: Initialize from a central store
+        self.store = {}
+        self.current_id = 0
+
+    def add_lot(self, lot, capacity):
+        self.store[lot] = ParkingLot(self.current_id, lot, capacity)
+        self.current_id += 1
+
+    def remove_lot(self, lot):
+        if lot in self.store:
+            del self.store[lot]
+
+    def increase_spots(self, lot):
+        if lot in self.store:
+            self.store[lot].increase_spots()
+
+    def decrease_spots(self, lot):
+        if lot in self.store:
+            self.store[lot].decrease_spots()
+
+    def get_capacity(self, lot):
+        if lot in self.store:
+            return self.store[lot].get_capacity()
+
+    def get_spots(self, lot):
+        if lot in self.store:
+            return self.store[lot].get_spots()
+
+    def set_boundary_lat(self, lot, coordinates):
+        self.store[lot].set_boundary_lot(coordinates)
+
+    def set_boundary_long(self, lot, coordinates):
+        self.store[lot].set_boundary_lot(coordinates)
+
+    def set_available_times(self, lot, available_times):
+        self.store[lot].set_boundary_lot(available_times)
+
+    def set_type(self, lot, type):
+        self.store[lot].set_boundary_lot(lot)
+
+    def get_store(self):
+        return self.store
+
+    def __repr__(self):
+        return self.store.__str__()
+
+    def __len__(self):
+        return len(self.store)
+
+    def __contains__(self, item):
+        return item in self.store
+
+
+class ParkingLot:
+
+    def __init__(self, id, name, capacity):
+        self.id = id
+        self.name = name
+        self.spots = capacity
+        self.capacity = capacity
+
+    def increase_spots(self):
+        self.spots = min(self.capacity, self.spots + 1)
+
+    def decrease_spots(self):
+        self.spots = max(0, self.spots - 1)
+
+    def get_spots(self):
+        return self.spots
+
+    def get_capacity(self):
+        return self.capacity
+
+    def reset(self):
+        self.spots = self.capacity
+
+    def set_boundary_lat(self, coordinates):
+        self.boundary_lat = coordinates
+
+    def set_boundary_long(self, coordinates):
+        self.boundary_long(self, coordinates)
+
+    def set_available_times(self, available_times):
+        self.available_times = available_times
+
+    def set_type(self, type):
+        self.type = type
+
+    def __repr__(self):
+        return "Lot(ID: {}, Name: {}, Spots: {}, Capacity: {})".format(self.id, self.name, self.spots, self.capacity)
+
+
+class StoreEncoder(JSONEncoder):
+
+    def default(self, o):
+        return o.__dict__
