13bc5e7a2390457499453ef71e0055f995f46717
Sanchit <sanchitcop19@Gmail.com>
2019-11-11 21:34:44 -0500

    Removed socket IO

--- patm-server/engine/routes.py
+++ patm-server/engine/routes.py
@@ -3,7 +3,6 @@
 ##############################################################################
 
 from flask import jsonify, redirect, url_for, request
-from flask_socketio import SocketIO, emit, join_room, leave_room
 from config import main_room
 from app import *
 
@@ -88,73 +87,6 @@ def register_user():
 # Sockets to be used as backup only
 ##############################################################################
 
-socketio = SocketIO()
-
-
-@socketio.on('enter')
-def enter(parking_lot):
-    """
-    Event triggered by the Raspberry PI when a car enters a lot
-    :return:
-    """
-    store, lot = app_instance.parking_store.get_store(), parking_lot["name"]
-    store[lot].decrease_spots()
-
-
-@socketio.on('exit')
-def exit(parking_lot):
-    """
-    Event triggered by the Raspberry PI when a car enters a lot
-    :return:
-    """
-    # TODO: Implement, to take care of failures don't allow decrements below 0
-    store, lot = app_instance.parking_store.get_store(), parking_lot["name"]
-    store[lot].increase_spots()
-
-
-@socketio.on('update')
-def update():
-    """
-    Client pings this method for a parking lot update
-    :return:
-    """
-    emit('status', {'store': StoreEncoder().encode(app_instance.parking_store)}, room=main_room, broadcast = True)
-
-
-@socketio.on('join')
-def join(message):
-    """
-    Sent by clients when they enter a room.
-    """
-    join_room(message['room'])
-    emit('status', {'store': StoreEncoder().encode(app_instance.parking_store)}, room=main_room, broadcast = False)
-
-
-@socketio.on('leave')
-def leave(message):
-    """
-    Sent by clients when they leave a room.
-    #TODO: this is probably not necessary
-    """
-    leave_room(main_room)
-
-
-@socketio.on('connect')
-def connect():
-    """
-    Both clients and raspberry pis trigger this method automatically when connecting
-    :return:
-    """
-    print(request.sid, " connected")
-
-
-@socketio.on('disconnect')
-def disconnect():
-    """
-    Both clients and raspberry pis trigger this method automatically when disconnecting
-    :return:
-    """
-    print(request.sid, " disconnected")
 
 
 def serialize_store():
